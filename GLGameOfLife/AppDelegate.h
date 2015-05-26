3a1dae328e571465359de8f4866e6a5e3fac6feb
Александр <aleksandr@mac.local>
2015-05-27 02:21:46 +0300

    Initial Commit

---
+++ GLGameOfLife/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GLGameOfLife
+//
+//  Created by Александр on 27.05.15.
+//  Copyright (c) 2015 Satori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GLGameOfLife/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GLGameOfLife
+//
+//  Created by Александр on 27.05.15.
+//  Copyright (c) 2015 Satori. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
